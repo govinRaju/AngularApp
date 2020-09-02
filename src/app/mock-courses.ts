080382e017d5566e59f4e2bd69a5f6bce1cf457b
Govind vuppu <vuppugovind3@gmail.com>
2020-09-02 17:46:15 -0500

    "Angular app initial commit"

---
+++ src/app/mock-courses.ts
@@ -0,0 +1,8 @@
+import { Course } from './course';
+
+export const COURSES: Course[] = [
+    { id: 1, name: "C++" },
+    { id: 2, name: "Jab" },
+    { id: 3, name: "Cat" },
+    { id: 4, name: "java" },
+    { id: 5, name: "Cot" }];
